88409799b8f021407e48c84af11796b28bcba6ff
alclol <lyuan1227@gmail.com>
2020-04-10 16:59:05 -0700

    fixed CMakeLists.txt

--- base_kvs.hpp
+++ base_kvs.hpp
@@ -1,7 +1,5 @@
-
-
-#include <unordered_map>
 #include "arugula/include/lattice_core.hpp"
+#include "arugula/include/merges/map_mrg.hpp"
 #include "arugula/include/merges/vector_clock_mrg.hpp"
 #include "arugula/include/merges/causal_mrg.hpp"
 
@@ -14,11 +12,11 @@ struct Value {
 template <typename K, typename V> 
 class KVStore {
 protected:
-  std::unordered_map<K, Lattice<Value<V>, CausalMerge> > store;
+  std::map<K, Lattice<Value<V>, CausalMerge> > store;
 
 public:
   KVStore<K, V>() {}
-  KVStore<K, V>(std::unordered_map<K, Lattice<Value<V>, CausalMerge>> &other) : store(other) { }
+  KVStore<K, V>(std::map<K, Lattice<Value<V>, CausalMerge>> &other) : store(other) { }
 
   Value<V> get(const K &k) {
     return store.at(k).reveal();
---
+++ tester.cpp
@@ -0,0 +1,9 @@
+//
+// Created by alclol on 4/10/20.
+//
+#include "base_kvs.hpp"
+
+int main(int argc, char** argv) {
+//   KVStore<> kvs;
+   return 0;
+}
