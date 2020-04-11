2a819dc0030a7d1e7a241f68f87b151b5e4c3742
alclol <lyuan1227@gmail.com>
2020-04-10 22:50:56 -0700

    test

--- tester.cpp
+++ tester.cpp
@@ -10,8 +10,12 @@ int main(int argc, char** argv) {
    std::string s1 = "Matthew";
    VectorClock vc1({{"x", Lattice(static_cast<unsigned>(2), Max{})},
                     {"y", Lattice(static_cast<unsigned>(4), Max{})}});
+   VectorClock vc2({{"x", Lattice(static_cast<unsigned>(3), Max{})},
+                    {"y", Lattice(static_cast<unsigned>(5), Max{})}});
    std::tuple<VectorClock, Lattice<int, Max>> v1(vc1, Lattice(777, Max{}));
+   std::tuple<VectorClock, Lattice<int, Max>> v2(vc2, Lattice(222, Max{}));
    kvs.put(s1, v1);
+   kvs.put(s1, v2);
    std::cout << kvs.get(s1).reveal() << std::endl;
    return 0;
 }
